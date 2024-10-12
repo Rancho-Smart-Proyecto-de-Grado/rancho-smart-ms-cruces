c63e7b55dbec2d2ab61139c6c9d2a2e9ed8b5fc9
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-12 02:59:27 -0500

    :sparkles: feat: Creación de la Aplicación SpringBoot.

---
+++ ms-cruces/src/main/java/com/rancho_smart/ms_cruces/MsCrucesApplication.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_cruces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCrucesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCrucesApplication.class, args);
+	}
+
+}
